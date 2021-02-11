2af5efaf8f86c79b67d13d74f1d61d36e56082de
dmdijumathew22 <dmdijumathew22@gmail.com>
2021-02-11 17:11:38 -0500

    user details from sql database using jdbc

--- src/main/java/com/hcl/SecurityConfiguration.java
+++ src/main/java/com/hcl/SecurityConfiguration.java
@@ -1,27 +1,32 @@
 package com.hcl;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
-
+	@Autowired
+	DataSource dataSource;
+	
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 	
-		auth.inMemoryAuthentication()
-			.withUser("tony")
-			.password("stark")
-			.roles("USER")
-			.and()
-			.withUser("Nick")
-			.password("fury")
-			.roles("ADMIN");
+		/*
+		 * auth.inMemoryAuthentication() .withUser("tony") .password("stark")
+		 * .roles("USER") .and() .withUser("Nick") .password("fury") .roles("ADMIN");
+		 */
+		auth.jdbcAuthentication()
+			.dataSource(dataSource)
+			.usersByUsernameQuery("select * from users where username = ? ")
+			.authoritiesByUsernameQuery("select username, authority from authorities where username = ?");
 		
 	}
 	
